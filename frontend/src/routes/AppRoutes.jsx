c4382cd83093727b6caa4e4c1f495f1cf5fefd06
Shemeera PS <shemeera.ps@schneideit.com>
2026-07-06 16:32:51 +0530

    Project Initialisation for people flow hrms frontend

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,10 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+function AppRoutes(){
+    return (<BrowserRouter>
+        <Routes>
+            
+        </Routes>
+    </BrowserRouter>)
+}
+
+export default AppRoutes;
